4b9181df01442a2d2b4ba3b96cdafe2cddf4a3d6
marvinstorage <marvinstorage@gmail.com>
2014-01-29 00:14:57 +0100

    Added Button, TextView and eventhandler

--- src/app/src/nl/healthjournal/MainActivity.java
+++ src/app/src/nl/healthjournal/MainActivity.java
@@ -1,15 +1,32 @@
 package nl.healthjournal;
 
+
+import org.w3c.dom.Text;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
 
+public class MainActivity 
+extends Activity 
+implements OnClickListener{
+	
+	private Button ok_Button;
+	private TextView myText;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ok_Button = (Button) this.findViewById(R.id.okButton);
+         myText = (TextView) this.findViewById(R.id.editText1);
+        ok_Button.setOnClickListener(this);
     }
 
 
@@ -19,5 +36,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		myText.setText("Health Logger test");
+		
+	}
     
 }
